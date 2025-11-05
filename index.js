fda3c369008a6dcf0c1dbe09144348fbe5f0286e
RaiyanSohel-byte <afnan19262@gmail.com>
2025-11-05 22:18:04 +0600

    api for jobs added

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.lyk9xse.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const jobsDB = client.db("jobsDB");
+const jobsCollection = jobsDB.collection("jobs");
+
+async function run() {
+  try {
+    await client.connect();
+
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = jobsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+    app.delete("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.delete("/jobs", async (req, res) => {
+      const result = await jobsCollection.deleteMany({});
+      res.send(result);
+    });
+    app.patch("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedJob = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedJob,
+      };
+      const result = await jobsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Mongo DB connected successfully");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
